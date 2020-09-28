5f455078c70231556ee4f38f896c08353a434bda
carlhumphreys <carl.humphreys@quotevineapp.com>
2020-09-28 11:56:51 +0100

    widening container on large screens

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ const Layout = ({ title = "Pokemon", children }) => {
                 <title>{title}</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className="container max-w-full md:max-w-3xl lg:max-w-4xl min-h-screen mx-auto pt-8 px-4 w-3xl">
+            <main className="container max-w-full md:max-w-3xl lg:max-w-6xl min-h-screen mx-auto pt-8 px-4 w-3xl">
                 {children}
             </main>
         </div>
