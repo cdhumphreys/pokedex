9611abb751592f31d7e616e7457dc65bfd3bca38
carlhumphreys <carl.humphreys@quotevineapp.com>
2020-09-28 11:57:19 +0100

    card styling

--- components/PokeCard.js
+++ components/PokeCard.js
@@ -6,23 +6,26 @@ import styles from "../styles/pokemon-types.module.scss";
 const PokeCard = ({ pokemon }) => {
     const { image, name, url, pokedexId } = pokemon;
     const mainTypeNames = pokemon.data.types.map((t) => t.type.name).slice(0, 2);
-    const typeClasses = mainTypeNames.reduce((acc, iter) => {
+    const bgClasses = mainTypeNames.reduce((acc, iter) => {
         return acc + "--" + iter;
     }, "bg");
+    const boxShadowClasses = mainTypeNames.reduce((acc, iter) => {
+        return acc + "--" + iter;
+    }, "boxShadow");
     return (
-        <div className={`p-4 my-2 border-2 rounded-md ${typeClasses}`}>
+        <div className={`relative p-4 my-2 border-2 rounded-md ${bgClasses} ${boxShadowClasses}`}>
             <Link href={`/pokemon?id=${pokedexId}`}>
                 <a className="flex flex-row items-center justify-between text-lg">
-                    <div className="flex flex-col items-start space-y-2">
-                        <h3 className="text-xl text-gray-600">#{pokedexId}</h3>
-                        <h2 className="text-2xl capitalize text-white">{name}</h2>
-                        <div className="grid grid-flow-row gap-1">
+                    <div className="flex flex-col items-start leading-none space-y-2">
+                        <h3 className="text-sm text-gray-800">#{pokedexId}</h3>
+                        <h2 className="text-2xl capitalize text-white font-bold">{name}</h2>
+                        <div className="grid gap-1 grid-cols-2 grid-flow-row">
                             {pokemon.data.types.map((t) => (
                                 <PokemonTypePill key={t.type.name} type={t} />
                             ))}
                         </div>
                     </div>
-                    <img className="w-32" src={image} alt={name} />
+                    <img className="-my-6 absolute right-0 top-0 w-1/3 md:h-full" src={image} alt={name} />
                 </a>
             </Link>
         </div>
