2bd9ffe52a909c258e492215cb011fc5162b1e78
Carl Humphreys <humphreys.carl.d@gmail.com>
2020-10-11 18:27:32 +0100

    btn styling

--- components/Button.js
+++ components/Button.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 const Button = ({ children, isPrimary, ...props }) => {
-    const classes = isPrimary ? "bg-red-500 text-white" : "bg-gray-400 text-gray-700";
+    const classes = isPrimary
+        ? "bg-red-500 text-white hover:bg-red-600"
+        : "bg-gray-400 text-gray-700 hover:bg-gray-500";
 
     return (
-        <button className={`flex p-4 rounded-lg ${classes}`} {...props}>
+        <button className={`flex py-3 px-3 rounded-lg focus:outline-none ${classes}`} {...props}>
             {children}
         </button>
     );
